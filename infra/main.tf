3579636bd42cace32c91de68695ef8e2e2bd0d2f
Jairo Júnior <jairojjr2003@gmail.com>
2026-07-13 13:15:01 -0300

    fix: app name invalid

--- infra/main.tf
+++ infra/main.tf
@@ -1,18 +1,19 @@
 locals {
-  name_prefix = var.app_name
+  app_name = lower(replace(var.app_name, " ", "-"))
+
   lambdas = {
     auth = {
-      function_name = "${local.name_prefix}-${var.environment}-auth"
+      function_name = "${local.app_name}-${var.environment}-auth"
       zip_path      = "${path.root}/dist/auth/function.zip"
     }
 
     user = {
-      function_name = "${local.name_prefix}-${var.environment}-user"
+      function_name = "${local.app_name}-${var.environment}-user"
       zip_path      = "${path.root}/dist/user/function.zip"
     }
 
     transaction = {
-      function_name = "${local.name_prefix}-${var.environment}-transaction"
+      function_name = "${local.app_name}-${var.environment}-transaction"
       zip_path      = "${path.root}/dist/transaction/function.zip"
     }
   }
@@ -20,24 +21,23 @@ locals {
 
 module "cognito" {
   source      = "./modules/cognito"
-  app_name    = var.app_name
+  app_name    = local.app_name
   environment = var.environment
 }
 
 module "iam" {
   source      = "./modules/iam"
-  app_name    = var.app_name
+  app_name    = local.app_name
   environment = var.environment
 }
 
 module "lambda" {
   for_each = local.lambdas
 
-  source           = "./modules/lambda"
-  app_name         = var.app_name
-  environment      = var.environment
-  function_name    = each.value.function_name
-  role_arn         = module.iam.lambda_role_arn
-  zip_path         = each.value.zip_path
-  # env_vars         = each.value.env_vars
+  source        = "./modules/lambda"
+  app_name      = local.app_name
+  environment   = var.environment
+  function_name = each.value.function_name
+  role_arn      = module.iam.lambda_role_arn
+  zip_path      = each.value.zip_path
 }
