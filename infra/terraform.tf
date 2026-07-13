54e5924d2aefeb544b2698b5954b954cb1e90666
Jairo Júnior <jairojjr2003@gmail.com>
2026-07-13 20:26:23 -0300

    feat: s3 for tf version

--- infra/terraform.tf
+++ infra/terraform.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+      bucket         = "money-planner-tfstate-124355634510"
+      key            = "dev/terraform.tfstate"
+      region         = "us-east-1"
+      dynamodb_table = "terraform-locks"
+      encrypt        = true
+    }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
